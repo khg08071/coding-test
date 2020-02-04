951d1eabec4d4bf5f17629009558b1d87cb9ca02
khg08071 <khg08071@hanyang.ac.kr>
2020-02-04 22:46:07 +0900

    copy

--- src/서울에서_김서방_찾기.java
+++ src/서울에서_김서방_찾기.java
@@ -1,14 +1,25 @@
 
 public class 서울에서_김서방_찾기 {
-	class Solution {
-		  public String solution(String[] seoul) {
+	//class Solution {
+		  public static String solution(String[] seoul) {
 		      String answer = "";
+		      int count = 0;
+		      for(String s : seoul) {
+		    	  if(s.equals("Kim")) {
+		    		  break;
+		    	  }
+		    	  count++;
+		      }
+		      answer += ("김서방은"+" "+count+"에 있다");
 		      return answer;
 		  }
-		}
+		//}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String[] seoul = {"Jane","Kim"};
+		System.out.println(solution(seoul));
+		
 
 	}
 
