24a906f7f69e6a78639dcfa9826d4d5f8e36e76c
HyungKyu Kim <khg08071@hanyang.ac.kr>
2020-02-04 13:16:57 +0900

    new

---
+++ src/소수찾기.java
@@ -0,0 +1,37 @@
+
+public class 소수찾기 {
+	class Solution {
+		  public int solution(int n) {
+		      int answer = 0;
+		      for(int i = 1; i<n;i++) {
+		    	  if(isPrime(i) == true) {
+		    		  answer +=1;
+		    	  }
+		      }
+		      
+		      return answer;
+		      
+		  
+		  }
+		  public boolean isPrime(int n) {
+			  int i = 2;
+			  boolean num = true;
+			  while(i<=n) {
+				  if(n%i == 0) {
+					  num = false;
+					  break;
+				  }
+				  i++;
+				  
+			  }
+			  return num;
+			 
+		  }
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/수박수박수박수박수박수.java
@@ -0,0 +1,25 @@
+
+public class 수박수박수박수박수박수 {
+	class Solution {
+		  public String solution(int n) {
+		      String answer = "";
+		      String p = "수";
+		      String q = "박";
+		      for(int i = 1; i <=n; i++) {
+		    	  if(i %2 ==0) {
+		    		  answer += q;
+		    	  }
+		    	  else {
+		    		  answer+=p;
+		    	  }
+		      }
+		      return answer;
+		  }
+		}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
