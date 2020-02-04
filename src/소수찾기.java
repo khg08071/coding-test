9a8ade4e5bfb42a1fa6916c514067ccdd497dc42
HyungKyu Kim <khg08071@hanyang.ac.kr>
2020-02-04 14:17:33 +0900

    new

--- src/소수찾기.java
+++ src/소수찾기.java
@@ -3,32 +3,28 @@ public class 소수찾기 {
 	class Solution {
 		  public int solution(int n) {
 		      int answer = 0;
-		      for(int i = 1; i<n;i++) {
+		      for(int i = 2; i<=n;i++) {
+                //if(i ==2 || i==3 ||i==5||i==7 ) answer +=1;
 		    	  if(isPrime(i) == true) {
 		    		  answer +=1;
 		    	  }
-		      }
-		      
+            }
 		      return answer;
-		      
-		  
 		  }
 		  public boolean isPrime(int n) {
 			  int i = 2;
 			  boolean num = true;
-			  while(i<=n) {
+			  while(i<n) {
 				  if(n%i == 0) {
 					  num = false;
 					  break;
 				  }
 				  i++;
-				  
 			  }
 			  return num;
 			 
 		  }
 		}
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
